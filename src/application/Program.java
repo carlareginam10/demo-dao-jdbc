070cd151c0f86f3585286162e0bb19e75edaa1f8
carla regina <carlareginam10@gmail.com>
2020-09-30 22:28:33 -0300

    implementação update

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = "+ newSeller.getId());
 		
+		System.out.println("=======================================================================================");
+		
+		System.out.println("=== TEST 5: Seller Update ====");	
+		seller = sellerDao.findByid(10);
+		seller.setName("Camila");
+		sellerDao.update(seller);
+		System.out.println("update complete");
 		
 
 		
